13558880d1bdc608efb66128947e6240f58f133a
yero13 <roman.yepifanov@gmail.com>
2017-09-05 15:05:08 +0300

    - where/order support for indexes in sprint backlog breakdown - changing access to class constants to private

--- jira/work.py
+++ jira/work.py
@@ -50,17 +50,17 @@ class Field:
 
 
 class SprintBacklogRequest(Request):
-    CFG_SPRINT_BACKLOG_REQUEST = './cfg/sprint-backlog-request.json'
-    KEY_BACKLOG_ITEMS = 'issues'
+    __CFG_SPRINT_BACKLOG_REQUEST = './cfg/sprint-backlog-request.json'
+    __KEY_BACKLOG_ITEMS = 'issues'
 
     def __init__(self, login, pswd):
-        with open(SprintBacklogRequest.CFG_SPRINT_BACKLOG_REQUEST) as cfg_file:
+        with open(SprintBacklogRequest.__CFG_SPRINT_BACKLOG_REQUEST) as cfg_file:
             Request.__init__(self, json.load(cfg_file, strict=False), login, pswd, is_multipage=True)
 
     def _parse_response(self, response, out_data):
         backlog = {}
-        self.__parse_field(response, self._response_cfg[SprintBacklogRequest.KEY_BACKLOG_ITEMS], backlog)
-        for issue in backlog[SprintBacklogRequest.KEY_BACKLOG_ITEMS]:
+        self.__parse_field(response, self._response_cfg[SprintBacklogRequest.__KEY_BACKLOG_ITEMS], backlog)
+        for issue in backlog[SprintBacklogRequest.__KEY_BACKLOG_ITEMS]:
             out_data.update({issue[Field.FIELD_KEY]:issue})
             self._logger.debug('issue: {}'.format(issue))
         return out_data
--- scrum/sprint.py
+++ scrum/sprint.py
@@ -7,19 +7,20 @@ class SprintBacklog:
     """
     Represents sprint backlog tasks. Provides functionality for checking sprint backlog consistency
     """
+    __CFG_SPRINT_BACKLOG_BREAKDOWN = './cfg/sprint-backlog-breakdown.json'
 
-    CFG_SPRINT_BACKLOG_BREAKDOWN = './cfg/sprint-backlog-breakdown.json'
-
-    CFG_KEY_INDEXES = "indexes"
-    CFG_KEY_INDEX_KEY = "key"
-    CFG_KEY_INDEX_VALUE = "value"
+    __CFG_KEY_INDEXES = 'indexes'
+    __CFG_KEY_INDEX_KEY = 'key'
+    __CFG_KEY_INDEX_VALUE = 'value'
+    __CFG_KEY_INDEX_WHERE = 'where'
+    __CFG_KEY_INDEX_ORDER = 'order'
 
     def __init__(self, work_dict):
         self.__logger = logging.getLogger(__name__)
         #self.__logger.info('sprint backlog input: {:d} work items'.format(len(work_dict)))
         self.__work_df = pd.DataFrame.from_dict(work_dict, orient='Index')
         #self.__logger.debug('backlog items for planning:\n {}'.format(list(self.__work_df.columns.values)))
-        with open(SprintBacklog.CFG_SPRINT_BACKLOG_BREAKDOWN) as cfg_file:
+        with open(SprintBacklog.__CFG_SPRINT_BACKLOG_BREAKDOWN) as cfg_file:
             self.__cfg = json.load(cfg_file, strict=False)
         self.__indexes = self.__create_indexes()
 
@@ -39,19 +40,33 @@ class SprintBacklog:
     def __create_indexes(self):
         indexes = []
         self.__logger.info('creating indexes')
-        for index in self.__cfg[SprintBacklog.CFG_KEY_INDEXES]:
-            index_cfg = self.__cfg[SprintBacklog.CFG_KEY_INDEXES][index]
-            key = index_cfg[SprintBacklog.CFG_KEY_INDEX_KEY]
-            value = index_cfg[SprintBacklog.CFG_KEY_INDEX_VALUE]
-            indexes.append(self.__create_index(index, key, value))
+        for index in self.__cfg[SprintBacklog.__CFG_KEY_INDEXES]:
+            index_cfg = self.__cfg[SprintBacklog.__CFG_KEY_INDEXES][index]
+            key = index_cfg[SprintBacklog.__CFG_KEY_INDEX_KEY]
+            value = index_cfg[SprintBacklog.__CFG_KEY_INDEX_VALUE]
+            where = index_cfg[
+                SprintBacklog.__CFG_KEY_INDEX_WHERE] if SprintBacklog.__CFG_KEY_INDEX_WHERE in index_cfg else None
+            order = index_cfg[
+                SprintBacklog.__CFG_KEY_INDEX_ORDER] if SprintBacklog.__CFG_KEY_INDEX_ORDER in index_cfg else None
+            indexes.append(self.__create_index(index, key, value, where, order))
         return indexes
 
-    def __create_index(self, name, key, value):
-        self.__logger.info('creating index {}: key {}, value {}'.format(name, key, value))
+    def __create_index(self, name, key, value, where, order):
+        self.__logger.info(
+            'creating index {}: key {}, value {}, where {}, order {}'.format(name, key, value, where, order))
         if self.__islist(key) and self.__islist(value):
             raise NotImplementedError('Creating index with more than one field of list type is not supported')
         # ToDo: implement checks for null/empty fields - now null/empty values for keys are filtered out
-        index_df = self.__work_df[[key, value]]
+        index_df = self.__work_df.query(where) if where else self.__work_df
+        if where:
+            self.__logger.debug(
+                'filtered: {:d} items from {:d} total'.format(index_df[key].count(), self.__work_df[key].count()))
+        index_df = index_df[[key, value]]
+        if order:
+            if self.__islist(key):
+                raise NotImplementedError('Custom order for list type key is not supported')
+            index_df[key] = index_df[key].astype('category')
+            index_df[key].cat.set_categories(order, inplace=True)
         if not self.__islist(key) and not self.__islist(value):
             index_df = index_df[index_df[key].notnull()]
             index = pd.Series().from_array(index_df[value].values, index=index_df[key], name=name)
@@ -65,7 +80,7 @@ class SprintBacklog:
                 index_df = index_df[index_df[value].apply(lambda value_list: True if len(value_list) > 0 else False)]
                 for item in index_df.iterrows():
                     index = index.append(pd.Series.from_array(item[1][value], index=[item[1][key]] * len(item[1][value])))
-
-        index = index.sort_index()
+        index.sort_index(inplace=True)
         self.__logger.debug('\n{}'.format(index))
+
         return index
