cf46afde85bb74c81c73d6c197854f7efc01bd18
yero13 <roman.yepifanov@gmail.com>
2017-09-05 16:53:56 +0300

    - modify fields support implemented

--- scrum/sprint.py
+++ scrum/sprint.py
@@ -9,11 +9,15 @@ class SprintBacklog:
     """
     __CFG_SPRINT_BACKLOG_BREAKDOWN = './cfg/sprint-backlog-breakdown.json'
 
-    __CFG_KEY_INDEXES = 'indexes'
+    __CFG_KEY_SCOPE_TRANSFORM = 'transform-scope'
+    __CFG_KEY_SCOPE_MODIFY_FIELDS = 'modify-fields'
+
+    __CFG_KEY_INDEXES = 'create-indexes'
     __CFG_KEY_INDEX_KEY = 'key'
     __CFG_KEY_INDEX_VALUE = 'value'
     __CFG_KEY_INDEX_WHERE = 'where'
     __CFG_KEY_INDEX_ORDER = 'order'
+
 
     def __init__(self, work_dict):
         self.__logger = logging.getLogger(__name__)
@@ -22,7 +26,8 @@ class SprintBacklog:
         #self.__logger.debug('backlog items for planning:\n {}'.format(list(self.__work_df.columns.values)))
         with open(SprintBacklog.__CFG_SPRINT_BACKLOG_BREAKDOWN) as cfg_file:
             self.__cfg = json.load(cfg_file, strict=False)
-        self.__indexes = self.__create_indexes()
+        # self.__indexes = self.__create_indexes() if SprintBacklog.__CFG_KEY_INDEXES in self.__cfg else []
+        self.__transform_scope()
 
     @classmethod
     def from_dict(cls, work_dict):
@@ -34,7 +39,15 @@ class SprintBacklog:
         """
         return cls(work_dict)
 
-    def __islist(self, field):
+    def __transform_scope(self):
+        if SprintBacklog.__CFG_KEY_SCOPE_MODIFY_FIELDS in self.__cfg[SprintBacklog.__CFG_KEY_SCOPE_TRANSFORM]:
+            changes = self.__cfg[SprintBacklog.__CFG_KEY_SCOPE_TRANSFORM][SprintBacklog.__CFG_KEY_SCOPE_MODIFY_FIELDS]
+            for field in changes:
+                self.__logger.info('modifying field \'{}\' - eval({})'.format(field, changes[field]))
+                self.__work_df.eval('{} = {}'.format(field, changes[field]), inplace=True)
+                self.__logger.debug('modified field:\n{}'.format(self.__work_df[field]))
+
+    def __is_list(self, field):
         return isinstance(self.__work_df[field].iloc[0], list)  # ToDo: define type based on first not null value
 
     def __create_indexes(self):
@@ -54,7 +67,7 @@ class SprintBacklog:
     def __create_index(self, name, key, value, where, order):
         self.__logger.info(
             'creating index {}: key {}, value {}, where {}, order {}'.format(name, key, value, where, order))
-        if self.__islist(key) and self.__islist(value):
+        if self.__is_list(key) and self.__is_list(value):
             raise NotImplementedError('Creating index with more than one field of list type is not supported')
         # ToDo: implement checks for null/empty fields - now null/empty values for keys are filtered out
         index_df = self.__work_df.query(where) if where else self.__work_df
@@ -63,20 +76,20 @@ class SprintBacklog:
                 'filtered: {:d} items from {:d} total'.format(index_df[key].count(), self.__work_df[key].count()))
         index_df = index_df[[key, value]]
         if order:
-            if self.__islist(key):
+            if self.__is_list(key):
                 raise NotImplementedError('Custom order for list type key is not supported')
             index_df[key] = index_df[key].astype('category')
             index_df[key].cat.set_categories(order, inplace=True)
-        if not self.__islist(key) and not self.__islist(value):
+        if not self.__is_list(key) and not self.__is_list(value):
             index_df = index_df[index_df[key].notnull()]
             index = pd.Series().from_array(index_df[value].values, index=index_df[key], name=name)
         else:
             index = pd.Series(name=name)
-            if self.__islist(key) and not self.__islist(value):
+            if self.__is_list(key) and not self.__is_list(value):
                 index_df = index_df[index_df[key].apply(lambda key_list: True if len(key_list) > 0 else False)]
                 for item in index_df.iterrows():
                     index = index.append(pd.Series.from_array([item[1][value]] * len(item[1][key]), index=item[1][key]))
-            elif not self.__islist(key) and self.__islist(value):
+            elif not self.__is_list(key) and self.__is_list(value):
                 index_df = index_df[index_df[value].apply(lambda value_list: True if len(value_list) > 0 else False)]
                 for item in index_df.iterrows():
                     index = index.append(pd.Series.from_array(item[1][value], index=[item[1][key]] * len(item[1][value])))
