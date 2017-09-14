08cc2a1b0c14a1e8b7693a68dbfa4953cfe35e43
yero13 <roman.yepifanov@gmail.com>
2017-09-14 12:06:45 +0300

    - Single request was implemented - Method GET is calling instead of POST - ToDo for GET/POST was added

--- jira/request.py
+++ jira/request.py
@@ -20,7 +20,6 @@ class Request():
 
     _KEY_RESPONSE = 'response'
 
-    # ToDo: split response and request cfg
     def __init__(self, cfg, login, pswd, is_multipage=False):
         self._logger = logging.getLogger(__name__)
         self.__login = login
@@ -37,31 +36,41 @@ class Request():
         return self.__response_values
 
     def __perform_single_request(self):
-        # ToDo:
-        return NotImplemented
+        response = self.__perform_request()
+        self._parse_response(response, self.__response_values)
 
     def __perform_multi_request(self):
         while True:
-            request_url = self.__request_cfg[Request.__KEY_REQUEST_URL]
-            request_data = self.__request_cfg[Request.__KEY_REQUEST_DATA]
-            self._logger.info('request {} from {}'.format(request_data, request_url))
-            response = requests.post(request_url,
-                                     json.dumps(request_data),
-                                     headers={"Content-Type": "application/json"},
-                                     auth=HTTPBasicAuth(self.__login, self.__pswd),
-                                     verify=True)
-            if not response.ok:
-                response.raise_for_status()
-            response = json.loads(response.content, strict=False)
+            response = self.__perform_request()
             self._parse_response(response, self.__response_values)
             total = int(response[Request.__KEY_PARAM_TOTAL])
             max_results = int(response[Request.__KEY_PARAM_MAX_RESULTS])
             start_at = int(response[Request.__KEY_PARAM_START_AT])
             start_at += max_results
             if start_at < total:
-                request_data.update({Request.__KEY_PARAM_START_AT: start_at})
+                self.__request_cfg[Request.__KEY_REQUEST_DATA].update({Request.__KEY_PARAM_START_AT: start_at})
                 continue
             break
+
+    def __get_request_params(self):
+        self.__request_url = self.__request_cfg[Request.__KEY_REQUEST_URL]
+        self.__request_data = self.__request_cfg[
+            Request.__KEY_REQUEST_DATA] if Request.__KEY_REQUEST_DATA in self.__request_cfg else None
+
+    def __perform_request(self):
+        request_url = self.__request_cfg[Request.__KEY_REQUEST_URL]
+        request_data = self.__request_cfg[
+            Request.__KEY_REQUEST_DATA] if Request.__KEY_REQUEST_DATA in self.__request_cfg else None
+        self._logger.info('request {} from {}'.format(request_data, request_url))
+        # ToDo: implement post/get methods
+        response = requests.get(request_url,
+                                request_data, # for post - json.dumps(self.__request_data),
+                                headers={"Content-Type": "application/json"},
+                                auth=HTTPBasicAuth(self.__login, self.__pswd),
+                                verify=True)
+        if not response.ok:
+            response.raise_for_status()
+        return json.loads(response.content, strict=False)
 
     @abc.abstractmethod
     def _parse_response(self, response, out_data):
