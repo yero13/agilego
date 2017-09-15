a5627cbfc849e601eb7d43e3d86f870d4425cf1e
yero13 <roman.yepifanov@gmail.com>
2017-09-15 11:05:11 +0300

    - sprint collection added - todos for future improvements

--- db/constants.py
+++ db/constants.py
@@ -1,9 +1,12 @@
+#ToDo: collection name wrapping should be moved to dest field in cfg file
 class DbConstants:
+    #Destination and collection prefix should be moved to cfg files
     SPRINT_COLLECTION_PREFIX = 'sprint'
     BACKLOG = 'backlog'
     BACKLOG_DETAILS = 'backlog-details'
     SUBTASKS = 'subtasks'
     SUBTASKS_DETAILS = 'substasks-details'
+    SPRINT = 'definition'
     ITEM_KEY = 'key'
     ITEM_PARENT = 'parent'
 
