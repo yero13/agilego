ebd8eba79af9061fc0c96015b965b85cdd81f4bf
yero13 <roman.yepifanov@gmail.com>
2017-09-29 16:34:27 +0300

    team stub removed

--- transform.py
+++ transform.py
@@ -3,7 +3,6 @@ import logging.config
 
 from services.constants import ApiConstants
 from db.connect import MongoDb
-from transformation.team import Team
 from transformation.transformer import Transformer
 
 CFG_LOG_TRANSFORM = './cfg/transform-logging-config.json'
@@ -19,6 +18,5 @@ if __name__ == '__main__':
         logger.info('Init transformer: {}'.format(CFG_TRANSFORM))
         with open(CFG_TRANSFORM) as cfg_file:
             Transformer(json.load(cfg_file, strict=False)).transform_data()
-        Team().to_db(db_dest) # Todo: remove
     except Exception as e:
         logging.error(e, exc_info=True)
