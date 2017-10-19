9d93d78cb742fcc4bbfae2dca84a335c6825909f
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2017-10-20 00:12:04 +0300

    unit tests draft

---
+++ agilego/test/test_import.py
@@ -0,0 +1,25 @@
+import unittest
+from integration.importer import Importer
+
+class ImportTestCase(unittest.TestCase):
+    def setUp(self):
+        #ToDo:
+        return
+
+    def tearDown(self):
+        #ToDo:
+        return
+
+"""
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(WidgetTestCase('test_default_widget_size'))
+    suite.addTest(WidgetTestCase('test_widget_resize'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+"""
