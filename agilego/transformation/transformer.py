88cc1704a4f8eb9c9858848392be3d13b74340fd
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2017-10-31 22:37:28 +0200

    update  transformation fix data generation - issues

--- agilego/transformation/transformer.py
+++ agilego/transformation/transformer.py
@@ -266,8 +266,10 @@ class UpdateTransformation(Transformation):
     def _transform(self):
         self.__df_dataset = pd.DataFrame.from_records(self.__dataset)
         if len(self.__update_data) > 0 and len(self.__dataset) > 0:
-            for field in self._transformation[UpdateTransformation.__CFG_KEY_UPD_FIELDS]:
-                self.__df_dataset[field] = self.__update_data[field]
+            for item in self._transformation[UpdateTransformation.__CFG_KEY_UPD_FIELDS]:
+                (src_field, dest_field), = item.items()
+                self.__df_dataset[dest_field] = self.__update_data[src_field]
+                #self._logger.debug('>>>>>>+++>>>>>>>> {}'.format(item))
 
     def _save(self):
         res = json.loads(self.__df_dataset.T.to_json()).values()
