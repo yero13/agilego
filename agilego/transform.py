b56e406918b1d7da870f086ff5ea28cc9e3032e3
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2017-11-02 01:29:35 +0200

    align transform with env params

--- agilego/transform.py
+++ agilego/transform.py
@@ -1,22 +1,22 @@
 import json
 import logging.config
-
-from db.connect import MongoDb
-from services.constants import ApiConstants
+from utils.cfg import CfgUtils
 from transformation.transformer import Transformer
 
 CFG_LOG_TRANSFORM = './cfg/log/transform-logging-config.json'
 CFG_TRANSFORM = './cfg/scrum/scrum-import-transform.json'
+CFG_ENV = './cfg/env.json'
 
 if __name__ == '__main__':
     with open(CFG_LOG_TRANSFORM) as logging_cfg_file:
         logging.config.dictConfig(json.load(logging_cfg_file, strict=False))
     logger = logging.getLogger(__name__)
-
-    db_dest = MongoDb(ApiConstants.CFG_DB_SCRUM_API).connection # Todo: remove
+    with open(CFG_ENV) as env_cfg_file:
+        env_cfg = json.load(env_cfg_file, strict=False)
     try:
         logger.info('Init transformer: {}'.format(CFG_TRANSFORM))
         with open(CFG_TRANSFORM) as cfg_file:
-            Transformer(json.load(cfg_file, strict=False)).transform_data()
+            cfg = json.loads(CfgUtils.substitute_params(cfg_file.read(), env_cfg))
+        Transformer(cfg).transform_data()
     except Exception as e:
         logging.error(e, exc_info=True)
