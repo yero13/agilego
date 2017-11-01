6b09ba4af5e018d8631396f2db4e2cc7779c11b6
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2017-11-02 01:03:54 +0200

    moving env params to file

--- agilego/import.py
+++ agilego/import.py
@@ -1,17 +1,21 @@
 import argparse
 import json
 import logging.config
-
+from utils.cfg import CfgUtils
 from integration.importer import Importer
 
 CFG_LOG_IMPORT = './cfg/log/import-logging-config.json'
 CFG_IMPORT = './cfg/jira/jira-import.json'
+CFG_ENV = './cfg/env.json'
 
 if __name__ == '__main__':
     with open(CFG_LOG_IMPORT) as log_cfg_file:
         logging.config.dictConfig(json.load(log_cfg_file, strict=False))
+    with open(CFG_ENV) as env_cfg_file:
+        env_cfg = json.load(env_cfg_file, strict=False)
     logger = logging.getLogger(__name__)
     parser = argparse.ArgumentParser()
+    # ToDo: get if absent in cfg
     parser.add_argument('--login', required=True)
     parser.add_argument('--pswd', required=True)
     args = parser.parse_args()
@@ -19,6 +23,7 @@ if __name__ == '__main__':
     try:
         logger.info('Init JIRA importer: {}'.format(CFG_IMPORT))
         with open(CFG_IMPORT) as cfg_file:
-            Importer(json.load(cfg_file, strict=False), args.login, args.pswd).perform()
+            cfg = json.loads(CfgUtils.substitute_params(cfg_file.read(), env_cfg))
+        Importer(cfg, args.login, args.pswd).perform()
     except Exception as e:
         logging.error(e, exc_info=True)
