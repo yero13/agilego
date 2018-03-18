055f56ac7dccca7547ab90113b03fc1b08b60fff
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2018-03-18 23:56:44 +0200

    v 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='agilego',
-      version='0.1',
+      version='0.1.0',
       description='Scrum sprint planner',
       author='Roman Yepifanov',
       url='https://github.com/yero13/agilego.be',
