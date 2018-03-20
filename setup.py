15d660fc5fb6646cee99618d0f62d317e5ecdc84
Roman Yepifanov <31592494+yero13@users.noreply.github.com>
2018-03-21 00:28:54 +0200

    rename import and transform

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(name='agilego',
       version='0.1.2',
-      description='Scrum sprint planner',
+      description='Sprint planner',
       author='Roman Yepifanov',
       url='https://github.com/yero13/agilego.be',
       license='GNU GPL v2.0',
